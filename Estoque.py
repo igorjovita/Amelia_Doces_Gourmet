85a98c43505b011465eaa1192f0db97fbe5885c6
igorjovita <igor.jovita@hotmail.com>
2023-11-10 20:21:15 -0300

    Update Estoque.py

--- Estoque.py
+++ Estoque.py
@@ -38,7 +38,7 @@ for sabor in sabores:
         st.subheader(sabor)
     with col2:
         mydb.connect()
-        cursor.execute(f"SELECT id FROM sabores WHERE nome = {sabor}")
+        cursor.execute(f"SELECT id FROM sabores WHERE nome = '{sabor}'")
         id_produto = str(cursor.fetchone()).translate(str.maketrans('', '', chars))
         cursor.execute(f"SELECT quantidade FROM estoque WHERE id_produto = {id_produto}")
         quantidade = str(cursor.fetchall()).translate(str.maketrans('', '', chars2)).split(',')
